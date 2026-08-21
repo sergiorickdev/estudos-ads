4be1a632da60ee433230659db0775209a4423f76
Sérgio <sergiohvds2026@gmail.com>
2026-08-21 17:33:58 -0300

    Adiciona Calculadora Simples com executável

--- python/calculadora_simples.py
+++ python/calculadora_simples.py
@@ -1,10 +1,16 @@
+import msvcrt
 print("Calculadora simples")
 numero1=float(input("Digite o primeiro número:"))
 numero2=float(input("Digite o segundo número:"))
 print("soma:", numero1+numero2)
 print("subtração:", numero1-numero2)
-print("multiplicação:", numero1 * numero2)
+print("multiplicação:", numero1*numero2)
 if numero2 != 0:
      print("divisão:", numero1/numero2)
 else:
      print("Não é possível dividir por zero.")
+print("Pressione ''Esc'' para fechar.")
+while True:
+        tecla=msvcrt.getch()
+        if tecla==b"\x1b":
+            break
